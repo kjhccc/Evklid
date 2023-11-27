56f3bc50a232bde0b4f7f721cbd379d2db12e534
Anton <kjhccc1@gmail.com>
2023-11-27 09:37:57 +0300

    Add js

---
+++ index.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', function(){
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+
+    },
+
+  });
+})
