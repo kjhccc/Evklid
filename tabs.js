1c3a2414b85b5779137e3cd1d4d1587e98153ade
Anton <kjhccc1@gmail.com>
2023-12-04 20:27:43 +0300

    Chete kogoto

---
+++ tabs.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', function(){
+
+  document.querySelectorAll('.btn-step').forEach(function(step){
+
+    step.addEventListener('click', function(event){
+
+      const path = event.currentTarget.dataset.path
+
+      document.querySelectorAll('.slider').forEach(function(slide){
+        slide.classList.remove('slide-active')
+
+      })
+
+      document.querySelector(`[data-target='${path}']`).classList.add('slide-active')
+    })
+  })
+
+})
